8cf339f22bd19edde05888ff3f234f702df19fab
Nishant Gupta <gupta.nishant08042004@gmail.com>
2026-06-19 16:59:57 +0530

    Initialize frontend with create-react-app structure
    - Added package.json with dependencies for React and testing libraries.
    - Created public assets including favicon, index.html, logos, manifest, and robots.txt.
    - Implemented basic styling in App.css and index.css.
    - Developed main App component with routing for Home and Admin Login pages.
    - Added Home and AdminLogin components for basic page structure.
    - Included reportWebVitals for performance tracking.
    - Set up testing environment with jest-dom for DOM assertions.

---
+++ frontend/src/pages/AdminLogin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AdminLogin = () => {
+  return (
+    <div className="d-flex ">
+        <div className='card p-4'>
+            Admin Login
+        </div>
+    </div>
+  )
+}
+
+export default AdminLogin
